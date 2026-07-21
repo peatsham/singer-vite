ca515a3fde52be8df37b5c2cb6608f56774e646d
peatsham <pete.eastham@gmail.com>
2026-07-21 14:26:16 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-swc' // or '@vitejs/plugin-react' if using standard react
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/singer-vite/',
+})
