28551dd5896ed678ed699cb18617d2564dccada9
peatsham <pete.eastham@gmail.com>
2026-09-23 16:53:43 +0100

    Fix Prettier formatting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/singer-vite/', // 👈 Make sure this line is exactly here!
+  base: "/singer-vite/", // 👈 Make sure this line is exactly here!
 });
-
